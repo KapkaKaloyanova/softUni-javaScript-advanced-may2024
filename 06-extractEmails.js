50ce91ce8a137b464d04755ac810bd71ccd0e8b2
Kapka Kaloyanova <146618996+KapkaKaloyanova@users.noreply.github.com>
2025-03-17 00:45:10 +0200

    Delete 06-extractEmails.js

--- 06-extractEmails.js
+++
@@ -1,6 +0,0 @@
-function extractEmails(data) {
-    let pattern = /(?<=\s|^)([A-Za-z0-9]+[A-Za-z0-9.\-_]*)@([a-z]+\-?[a-z]+)(\.[a-z]+)+/g;
-    let emails = data.match(pattern);
-    emails.forEach(x => console.log(x));
-}
-extractEmails('Please contact us at: support@github.com. Just send email to s.miller@mit.edu and j.hopking@york.ac.uk for more information.')
